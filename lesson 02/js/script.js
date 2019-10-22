3c97e04fbf809d2d4cc12b511e1deec60e09e92b
ValidAbdr <89277114142@mail.ru>
2019-10-23 02:40:55 +0400

    сделан второй урок

---
+++ lesson 02/js/script.js
@@ -0,0 +1,17 @@
+let money = 33000,
+		income = 'электрик',
+		addExpenses = 'Коммуналка, дорога, питание' ,
+		deposit = true,
+		mission = 55300,
+		period = 3,
+		budgetDay = (money / 30),
+		budgetDayTwo = (money % 30);
+
+// alert ('Привет первому занятию' );
+console.log(money, income, deposit);
+console.log(income.length)
+console.log("Период " + period + " месяцев");
+console.log('Цель заработать ' +mission + ' руб');
+console.log(addExpenses.toLowerCase().split(' '));
+
+console.log('World peace ;)');
