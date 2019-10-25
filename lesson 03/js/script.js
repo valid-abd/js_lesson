ddce20c3ce4d798b9c2d1572b0d235300f5aaf6b
ValidAbdr <89277114142@mail.ru>
2019-10-25 22:07:03 +0400

    сделано 3 задание

---
+++ lesson 03/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let money = +prompt('Ваш месячный доход?'),
+		income = 'электрик',
+		addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую').split((',')),
+		deposit = confirm('Есть ли у вас депозит в банке?');
+		mission = 553000;
+		// budgetDayTwo = (money % 30);
+
+// alert ('Привет первому занятию' );
+console.log(money);
+console.log(addExpenses);
+console.log(deposit);
+
+let questions1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let questions2 = +prompt('Во сколько это обойдется?');
+
+let questions3 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let questions4 = +prompt('Во сколько это обойдется?');
+
+console.log('questions1: ', questions1);
+console.log('questions2: ', questions2);
+console.log('questions3: ', questions3);
+console.log('questions4: ', questions4);
+
+let budgetMonth = money - questions2 + questions4;  // доход за месяц
+console.log('budgetMonth: ', budgetMonth); 
+
+let period = mission / budgetMonth
+console.log(Math.ceil(period));
+
+let budgetDay = (budgetDay / 30);
+console.log(Math.floor(budgetDay));
+
+let option = (budgetDay >= 800) ? ' Высокий уровень дохода ' :
+	(budgetDay >= 300) ? ' Средний уровень дохода ' :
+	(budgetDay >= 0) ? 'Низкий уровень дохода' :
+
+	' Что то пошло не так ';
+
+console.log(option);
